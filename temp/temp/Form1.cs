2e41ba98ece376a9a3a4d5b50a09a2111b450fb0
stasya <mail@gmail.com>
2015-12-25 09:01:24 +0200

    usgufysydufsdf

--- temp/temp/Form1.cs
+++ temp/temp/Form1.cs
@@ -16,5 +16,10 @@ namespace temp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fysuafysaf
+        }
     }
 }
